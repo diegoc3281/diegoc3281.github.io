4d785995e7cfb250ba4a8726605eb3a20e082f88
diegoc3281 <91745372+diegoc3281@users.noreply.github.com>
2021-10-01 11:16:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Diego Calixto"
+let userName = "diegoc3281";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
