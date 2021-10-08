e2aeef877b9c51ee1b353ae01273e3d2f60b886b
chinchunc9858 <92172015+chinchunc9858@users.noreply.github.com>
2021-10-08 11:31:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Chin Chun Chen"; // update the part BETWEEN the "quotes"
+let userName = "chinchunc9858"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
